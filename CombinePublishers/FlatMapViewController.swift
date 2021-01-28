9dcc37cb4cf7312fef7978432b9fc041089d0d76
Naz <iamnazdelacruz@gmail.com>
2021-01-28 21:44:37 +0800

    Update flatmap operator example

--- CombinePublishers/FlatMapViewController.swift
+++ CombinePublishers/FlatMapViewController.swift
@@ -5,24 +5,25 @@
 //  Created by naz on 1/24/21.
 //
 
-import UIKit
 import Combine
+import UIKit
 /*
-{
-  "userId": 1,
-  "id": 1,
-  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-}
- 
- https://jsonplaceholder.typicode.com/posts/
- */
+ {
+   "userId": 1,
+   "id": 1,
+   "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+   "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+ }
 
+  https://jsonplaceholder.typicode.com/posts/1
+  */
 
+// Query Object
 struct PostQuery {
     let id: String
 }
 
+// Result Representation
 struct Post: Decodable {
     let id: Int
     let title: String
@@ -30,18 +31,21 @@ struct Post: Decodable {
 }
 
 class FlatMapViewController: UIViewController {
-    var blogPublisher = PassthroughSubject<PostQuery, URLError>()
-    var cancellable: AnyCancellable!
-    
     init() {
         super.init(nibName: nil, bundle: nil)
-        self.view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemBackground
     }
-    
+
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
+    var blogPublisher = PassthroughSubject<PostQuery, URLError>()
+
+    var cancellable: AnyCancellable!
+    var cancellables = Set<AnyCancellable>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -50,24 +54,22 @@ class FlatMapViewController: UIViewController {
             return URLSession.shared.dataTaskPublisher(for: url)
         }.eraseToAnyPublisher()
         
-        cancellable = publisher.map {
+        publisher.map {
             $0.data
         }
         .decode(type: Post.self, decoder: JSONDecoder())
-        .sink { (completion) in
+        .sink { completion in
             switch completion {
             case .finished:
                 print("finished")
             case .failure(let error):
                 print(error)
             }
-        } receiveValue: { value in
-            print(value.id)
-        }
-        
+        } receiveValue: { output in
+            print(output)
+        }.store(in: &cancellables)
+
         blogPublisher.send(PostQuery(id: "1"))
         blogPublisher.send(PostQuery(id: "2"))
-        blogPublisher.send(PostQuery(id: "3"))
     }
 }
-
